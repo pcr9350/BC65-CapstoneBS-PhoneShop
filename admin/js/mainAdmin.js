fee12ba0744f4c13053ed8e5728c3c5cce864344
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-03-31 18:11:48 +0700

    complete DELETE and POST sản phẩm trang admin

--- admin/js/mainAdmin.js
+++ admin/js/mainAdmin.js
@@ -9,3 +9,33 @@ function fetchProductList() {
     })
 }
 fetchProductList()
+
+function deleteProduct(id){
+    if (window.confirm('Bạn có muốn xóa sản phẩm này')) {
+        productServ.deleteProductById(id).then(function(response){
+            console.log('response:', response.data)
+            fetchProductList()
+    
+        }).catch(function(error){
+            console.log('error:', error)
+    
+        })
+    } else {
+        console.log('Người dùng đã hủy bỏ xóa sản phẩm')
+    }
+}
+
+function addProduct(){
+    var sp = getInfo()
+    console.log('sp:', sp)
+
+    productServ.addNewProduct(sp).then(function(response){
+        console.log('response:', response)
+        $('#myModal').modal('hide');
+        fetchProductList()
+
+    }).catch(function(error){
+        console.log('error:', error)
+
+    })
+}
