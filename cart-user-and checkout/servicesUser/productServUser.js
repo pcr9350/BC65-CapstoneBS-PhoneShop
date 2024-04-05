b1c9098335f8272b0bb9b0821c3fecefad58b3dd
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-04-05 22:57:55 +0700

    user cart GET api product, thêm tính price ở layout cart, check out, add product

---
+++ cart-user-and checkout/servicesUser/productServUser.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://66058c5b2ca9478ea1809fcb.mockapi.io/Products'
+
+var productServ = {
+    getProduct: function () {
+        return axios({
+            url: BASE_URL,
+            method: 'GET'
+        })
+    }
+}
